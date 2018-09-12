8f14acefe30e49e1f473845e6fa7ee54018d96aa
TheOriginalCreator <wisdomdancelove@gmail.com>
2018-09-12 16:27:12 +0100

    My final commit for the day

--- js/script.js
+++ js/script.js
@@ -1,7 +1,53 @@
 let canvas = document.getElementById("gameCanvas");
 let ctx = canvas.getContext("2d");
 
-let img = new Image()
-img.src = './sprites/enemy.png'
+let img = new Image();
+let img2 = new Image();
 
-ctx.drawImage(img, 20, 20, 20, 20)
+let x = 1;
+let y = 10;
+
+let dx = 10;
+let dy = 50;
+
+let spriteRadius = 25;
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawEnemy();
+
+    x += dx;
+
+    if(x + dx >= 150 || x + dx < 10) {
+        dx = -dx;
+        y += 5
+    }
+}
+
+function drawEnemy() {
+    img.onload = function () {
+        let enemy1 = ctx.drawImage(img, x, y); // source , left/right, up/down, width, height 
+        
+        let i = x + 30
+        let enemy2 = ctx.drawImage(img, i, y); // source , left/right, up/down, width, height 
+
+        let u = x + 60
+        let enemy3 = ctx.drawImage(img, u, y); // source , left/right, up/down, width, height 
+
+        let o = x + 90
+        let enemy4 = ctx.drawImage(img, o, y); // source , left/right, up/down, width, height
+        
+        let q = x + 120
+        let enemy5 = ctx.drawImage(img, q, y); // source , left/right, up/down, width, height
+        
+        if (y == 30) {
+            enemy5 = ctx.Image(img, 500, 500);
+        }
+    }
+
+    img.src = '../sprites/enemy.png';
+}
+
+
+setInterval(draw, 170);
+drawBlocks()
